49eadeae2a34c42ae352b4d6b987c9dbd7cb797c
AneeMandmets <92388991+AneeMandmets@users.noreply.github.com>
2022-05-07 19:37:13 +0300

    Update menyy.py

--- menyy.py
+++ menyy.py
@@ -2,9 +2,11 @@ import PySimpleGUI as sg
 import mysql.connector
 from mysql.connector import connect, Error
 from kuva2 import *
+from lisamine import *
+from kustutamine import *
 
 def kuvamine():
-    layout = [[sg.Text("Lillede andmebaas\n Vali lilled, mida kuvada:")],
+    layout = [[sg.Text("Lillede andmebaas\nVali lilled, mida kuvada:")],
     [sg.Button("Kõik")], 
     [sg.Button("Hostad")],
     [sg.Button("Iirised")],
@@ -15,7 +17,8 @@ def kuvamine():
     [sg.Button("Nartsissid")],
     [sg.Button("Laugud")],
     [sg.Button("Kannad")],
-    [sg.Button("Ilupuud ja põõsad")],
+    [sg.Button("Ilupuud ja -põõsad")],
+    [sg.Button("Muu")],
     ]
 
     window = sg.Window("Andmebaas", layout)
@@ -41,8 +44,100 @@ def kuvamine():
             kuva_lauk()
         elif event == "Kannad":
             kuva_kanna()
-        elif event == "Ilupuud ja põõsad":
+        elif event == "Ilupuud ja -põõsad":
             kuva_ip()
+        elif event == "Muu":
+            kuva_muu()
+        elif event == sg.WIN_CLOSED:
+            break
+
+    window.close()
+
+def lisamine():
+    layout = [[sg.Text("Lillede andmebaas\nVali lill, mida lisada:")], 
+    [sg.Button("Hosta")],
+    [sg.Button("Iiris")],
+    [sg.Button("Päevaliilia")],
+    [sg.Button("Liilia")],
+    [sg.Button("Jorjen")],
+    [sg.Button("Tulp")],
+    [sg.Button("Nartsiss")],
+    [sg.Button("Lauk")],
+    [sg.Button("Kanna")],
+    [sg.Button("Ilupuu või -põõsas")],
+    [sg.Button("Muu")],
+    ]
+
+    window = sg.Window("Andmebaas", layout)
+    while True:
+        event, values = window.read()
+        if event == "Hosta":
+            lisa_hosta()
+        elif event == "Iiris":
+            lisa_iiris()
+        elif event == "Päevaliilia":
+            lisa_paevaliilia()
+        elif event == "Liilia":
+            lisa_liilia()
+        elif event == "Jorjen":
+            lisa_jorjen()
+        elif event == "Tulp":
+            lisa_tulp()
+        elif event == "Nartsiss":
+            lisa_nartsiss()
+        elif event == "Lauk":
+            lisa_lauk()
+        elif event == "Kanna":
+            lisa_kanna()
+        elif event == "Ilupuu või -põõsas":
+            lisa_ip()
+        elif event == "Muu":
+            lisa_muu()
+        elif event == sg.WIN_CLOSED:
+            break
+
+    window.close()
+
+def kustutamine():
+    layout = [[sg.Text("Lillede andmebaas\nVali lill, mida kustutada:")], 
+    [sg.Button("Hosta")],
+    [sg.Button("Iiris")],
+    [sg.Button("Päevaliilia")],
+    [sg.Button("Liilia")],
+    [sg.Button("Jorjen")],
+    [sg.Button("Tulp")],
+    [sg.Button("Nartsiss")],
+    [sg.Button("Lauk")],
+    [sg.Button("Kanna")],
+    [sg.Button("Ilupuu või -põõsas")],
+    [sg.Button("Muu")],
+    ]
+
+    window = sg.Window("Andmebaas", layout)
+    while True:
+        event, values = window.read()
+        if event == "Hosta":
+            kustuta_hosta()
+        elif event == "Iiris":
+            kustuta_iiris()
+        elif event == "Päevaliilia":
+            kustuta_paevaliilia()
+        elif event == "Liilia":
+            kustuta_liilia()
+        elif event == "Jorjen":
+            kustuta_jorjen()
+        elif event == "Tulp":
+            kustuta_tulp()
+        elif event == "Nartsiss":
+            kustuta_nartsiss()
+        elif event == "Lauk":
+            kustuta_lauk()
+        elif event == "Kanna":
+            kustuta_kanna()
+        elif event == "Ilupuu või -põõsas":
+            kustuta_ip()
+        elif event == "Muu":
+            kustuta_muu()
         elif event == sg.WIN_CLOSED:
             break
 
