e6aaa73ada6dd5f68e86fe3dd90616e7aaf78512
AneeMandmets <92388991+AneeMandmets@users.noreply.github.com>
2022-05-07 19:36:29 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -3,18 +3,29 @@ import mysql.connector
 from mysql.connector import connect, Error
 from kuva2 import *
 from menyy import *
+from lisamine import *
+from muutmine import *
+
 
 layout = [[sg.Text("Lillede andmebaas\nVali tegevus, mida teha:")], 
 [sg.Button("Kuvamine")],
 [sg.Button("Lisamine")],
+[sg.Button("Kustutamine")],
+[sg.Button("Muutmine")],
 ]
 
-window = sg.Window("Andmebaas", layout)
+window = sg.Window("Andmebaas", layout, resizable=True)
 while True:
     event, values = window.read()
     if event == "Kuvamine":
         kuvamine()
+    elif event == "Lisamine":
+        lisamine()
+    elif event == "Kustutamine":
+        kustutamine()
+    elif event == "Muutmine":
+        muutmine()
     elif event == sg.WIN_CLOSED:
-            break
+        break
 
 window.close()
