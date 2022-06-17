94dfd2911fb8636e2fb8f69d4da67004294911f4
AneeMandmets <92388991+AneeMandmets@users.noreply.github.com>
2022-06-17 17:27:26 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -5,13 +5,16 @@ from kuva2 import *
 from menyy import *
 from lisamine import *
 from muutmine import *
+from otsimine import *
 
-
-layout = [[sg.Text("Lillede andmebaas\nVali tegevus, mida teha:")], 
+layout = [[sg.Text("Lillede andmebaas\nVali tegevus, mida teha:")],
+[sg.Text("_" * 40)], 
 [sg.Button("Kuvamine")],
+[sg.Button("Otsimine")],
 [sg.Button("Lisamine")],
 [sg.Button("Kustutamine")],
 [sg.Button("Muutmine")],
+[sg.Button("Lahkumine")],
 ]
 
 window = sg.Window("Andmebaas", layout, resizable=True)
@@ -19,13 +22,18 @@ while True:
     event, values = window.read()
     if event == "Kuvamine":
         kuvamine()
+    elif event == "Otsimine":
+        otsimine()
     elif event == "Lisamine":
         lisamine()
     elif event == "Kustutamine":
         kustutamine()
     elif event == "Muutmine":
         muutmine()
+    elif event == "Lahkumine":
+        window.close()
     elif event == sg.WIN_CLOSED:
         break
 
 window.close()
+
