17ec2ae84f829bbd3b400abf8e947940b2b3c14a
AneeMandmets <92388991+AneeMandmets@users.noreply.github.com>
2022-06-17 17:27:06 +0300

    Update lisamine.py

--- lisamine.py
+++ lisamine.py
@@ -233,6 +233,29 @@ def lisa_ip():
             window.close()
     window.close()
 
+def lisa_rodo():
+    con = mysql.connector.connect(host ="localhost",user = "root",password = 'L4ste4i40pet4j4',database='lilled')
+    cursor = con.cursor()
+    sqlquery = "INSERT INTO nimi (nimi, liik_idliik) VALUES (%s, %s)"
+    val = 12
+
+    layout = [[sg.Text("Rododendroni lisamine")], 
+    [sg.Text("_" * 20)],
+    [sg.Text("Nimi", size=(15, 1)), sg.InputText(key='NIMI')],
+    [sg.Button("OK"), sg.Button("Katkesta")],
+    ]
+
+    window = sg.Window("Andmebaas", layout)
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == "Katkesta":
+            break
+        elif event == "OK":
+            cursor.execute(sqlquery, (values['NIMI'], val))
+            con.commit()
+            window.close()
+    window.close()
+
 def lisa_muu():
     con = mysql.connector.connect(host ="localhost",user = "root",password = 'L4ste4i40pet4j4',database='lilled')
     cursor = con.cursor()
@@ -252,6 +275,51 @@ def lisa_muu():
             break
         elif event == "OK":
             cursor.execute(sqlquery, (values['NIMI'], val))
+            con.commit()
+            window.close()
+    window.close()
+
+def lisa_maitsetaim():
+    con = mysql.connector.connect(host ="localhost",user = "root",password = 'L4ste4i40pet4j4',database='lilled')
+    cursor = con.cursor()
+    sqlquery = "INSERT INTO nimi (nimi, liik_idliik) VALUES (%s, %s)"
+    val = 13
+
+    layout = [[sg.Text("Maitsetaime lisamine")], 
+    [sg.Text("_" * 20)],
+    [sg.Text("Nimi", size=(15, 1)), sg.InputText(key='NIMI')],
+    [sg.Button("OK"), sg.Button("Katkesta")],
+    ]
+
+    window = sg.Window("Andmebaas", layout)
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == "Katkesta":
+            break
+        elif event == "OK":
+            cursor.execute(sqlquery, (values['NIMI'], val))
+            con.commit()
+            window.close()
+    window.close()
+
+def lisa_liik():
+    con = mysql.connector.connect(host ="localhost",user = "root",password = 'L4ste4i40pet4j4',database='lilled')
+    cursor = con.cursor()
+    sqlquery = "INSERT INTO liigid (liik) VALUES (%s)"
+
+    layout = [[sg.Text("Uue liigi lisamine")],  
+    [sg.Text("_" * 20)],
+    [sg.Text("Liigi nimi", size=(15, 1)), sg.InputText(key='NIMI')],
+    [sg.Button("OK"), sg.Button("Katkesta")],
+    ]
+
+    window = sg.Window("Andmebaas", layout)
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == "Katkesta":
+            break
+        elif event == "OK":
+            cursor.execute(sqlquery, (values['NIMI'],))
             con.commit()
             window.close()
     window.close()
