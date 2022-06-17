3dca938f9de96a1b92c001a53dc76b6648ac3c39
AneeMandmets <92388991+AneeMandmets@users.noreply.github.com>
2022-06-17 17:26:46 +0300

    Update kuva2.py

--- kuva2.py
+++ kuva2.py
@@ -256,6 +256,52 @@ def kuva_ip():
 
     window.close()
 
+def kuva_rodo():
+    headings = ['ID', 'NIMI']
+
+    con = mysql.connector.connect(host ="localhost",user = "root",password = 'L4ste4i40pet4j4',database='lilled')
+    cursor = con.cursor()
+    sqlquery = "SELECT idnimi, nimi FROM nimi WHERE liik_idliik = '12'"
+    cursor.execute(sqlquery)
+
+    lilled = cursor.fetchall()
+    rows_affected = cursor.rowcount
+
+    layout = [[sg.Table(lilled, headings = headings, justification="center", key="-LILLED-")],
+    [sg.Text("Lilli kokku:"), sg.Text(rows_affected)],]
+    window = sg.Window("Lillede andmebaas", layout, finalize=True)
+
+    while True:
+        event, values = window.read()
+        if event==sg.WINDOW_CLOSED:
+            break
+        print(event, values)
+
+    window.close()
+
+def kuva_maitsetaim():
+    headings = ['ID', 'NIMI']
+
+    con = mysql.connector.connect(host ="localhost",user = "root",password = 'L4ste4i40pet4j4',database='lilled')
+    cursor = con.cursor()
+    sqlquery = "SELECT idnimi, nimi FROM nimi WHERE liik_idliik = '13'"
+    cursor.execute(sqlquery)
+
+    lilled = cursor.fetchall()
+    rows_affected = cursor.rowcount
+
+    layout = [[sg.Table(lilled, headings = headings, justification="center", key="-LILLED-")],
+    [sg.Text("Lilli kokku:"), sg.Text(rows_affected)],]
+    window = sg.Window("Lillede andmebaas", layout, finalize=True)
+
+    while True:
+        event, values = window.read()
+        if event==sg.WINDOW_CLOSED:
+            break
+        print(event, values)
+
+    window.close()
+
 def kuva_muu():
     headings = ['ID', 'NIMI']
 
@@ -269,6 +315,29 @@ def kuva_muu():
 
     layout = [[sg.Table(lilled, headings = headings, justification="center", key="-LILLED-")],
     [sg.Text("Lilli kokku:"), sg.Text(rows_affected)],]
+    window = sg.Window("Lillede andmebaas", layout, finalize=True)
+
+    while True:
+        event, values = window.read()
+        if event==sg.WINDOW_CLOSED:
+            break
+        print(event, values)
+
+    window.close()
+
+def kuva_liigid():
+    headings = ['ID', 'NIMI']
+
+    con = mysql.connector.connect(host ="localhost",user = "root",password = 'L4ste4i40pet4j4',database='lilled')
+    cursor = con.cursor()
+    sqlquery = "SELECT idliik, liik FROM liigid"
+    cursor.execute(sqlquery)
+
+    lilled = cursor.fetchall()
+    rows_affected = cursor.rowcount
+
+    layout = [[sg.Table(lilled, headings = headings, justification="center", key="-LIIGID-")],
+    [sg.Text("Liike kokku:"), sg.Text(rows_affected)],]
     window = sg.Window("Lillede andmebaas", layout, finalize=True)
 
     while True:
