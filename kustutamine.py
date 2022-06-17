ab3302b7fe745f09a08e0d5f9a28001bfb7947c4
AneeMandmets <92388991+AneeMandmets@users.noreply.github.com>
2022-06-17 17:24:52 +0300

    Update kustutamine.py

--- kustutamine.py
+++ kustutamine.py
@@ -2,7 +2,6 @@ import PySimpleGUI as sg
 import mysql.connector
 from mysql.connector import connect, Error
 from tkinter import READABLE
-
 from kuva2 import *
 
 def kustuta_hosta():
@@ -233,6 +232,54 @@ def kustuta_ip():
 
     window = sg.Window("Andmebaas", layout)
     kuva_ip()
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == "Katkesta":
+            break
+        elif event == "OK":
+
+            cursor.execute(sqlquery, (values['ID'],))
+            con.commit()
+            window.close()
+    window.close()
+
+def kustuta_rodo():
+    con = mysql.connector.connect(host ="localhost",user = "root",password = 'L4ste4i40pet4j4',database='lilled')
+    cursor = con.cursor()
+    sqlquery = "DELETE FROM nimi WHERE idnimi = %s"
+
+    layout = [[sg.Text("Rododendroni kustutamine")], 
+    [sg.Text("_" * 20)],
+    [sg.Text("Sisesta ID", size=(15, 1)), sg.Input(key='ID')],
+    [sg.Button("OK"), sg.Button("Katkesta")],
+    ]
+
+    window = sg.Window("Andmebaas", layout)
+    kuva_rodo()
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == "Katkesta":
+            break
+        elif event == "OK":
+
+            cursor.execute(sqlquery, (values['ID'],))
+            con.commit()
+            window.close()
+    window.close()
+
+def kustuta_maitsetaim():
+    con = mysql.connector.connect(host ="localhost",user = "root",password = 'L4ste4i40pet4j4',database='lilled')
+    cursor = con.cursor()
+    sqlquery = "DELETE FROM nimi WHERE idnimi = %s"
+
+    layout = [[sg.Text("Maitsetaime kustutamine")], 
+    [sg.Text("_" * 20)],
+    [sg.Text("Sisesta ID", size=(15, 1)), sg.Input(key='ID')],
+    [sg.Button("OK"), sg.Button("Katkesta")],
+    ]
+
+    window = sg.Window("Andmebaas", layout)
+    kuva_rodo()
     while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED or event == "Katkesta":
